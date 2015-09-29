0640d225cb0d0ad95d8d868f79c7e43bf85204d9
andrepitombeira <andrepitombeira@gmail.com>
2015-09-29 18:02:52 +0200

    Project creation

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('remotetalks', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.bootstrap', 'toastr']);
+
+})();
